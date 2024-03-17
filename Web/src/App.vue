11cd45f0f6c45b5cfa85cbac265fbde0081839b8
Abel Wang <wanghaixu01@sina.com>
2024-03-18 01:13:25 +0800

    init frontend and backend interaction

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <div>
+      Hello World
+      <div>Bot 昵称: {{ botName }}</div>
+      <div>Bot 战力: {{ botRating }}</div>
+    </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+export default {
+  name: 'App',
+  setup: ()=> {
+    let botName = ref('');
+    let botRating = ref('');
+    $.ajax({
+      url: 'http://localhost:3000/pk/getbotinfo',
+      type: 'GET',
+      success: resp => {
+        botName.value = resp[0].name;
+        botRating.value = resp[0].rating;
+        console.log(resp);
+      }
+    });
+
+    return {
+      botName,
+      botRating
+    }
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
